8fbfaca56fbf59faa468ff2b9f46a3ff4a3888ad
donghaotian <you@example.com>
2019-05-10 21:10:45 +0800

    shopping

--- wechat/pages/shopping/index/open.js
+++ wechat/pages/shopping/index/open.js
@@ -1,66 +1,57 @@
-// pages/shopping/index/open.js
+const app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    ColorList: app.globalData.ColorList,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  SetRound(e) {
+    this.setData({
+      round: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  SetSize(e) {
+    this.setData({
+      size: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  SetColor(e) {
+    this.setData({
+      color: e.currentTarget.dataset.color,
+      modalName: null
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  SetShadow(e) {
+    this.setData({
+      shadow: e.detail.value
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  SetBorder(e) {
+    this.setData({
+      border: e.detail.value
+    })
+    if (!e.detail.value) {
+      this.setData({
+        bordersize: false
+      })
+    }
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  SetBorderSize(e) {
+    this.setData({
+      bordersize: e.detail.value
+    })
+  },
+  SetBlock(e) {
+    this.setData({
+      block: e.detail.value
+    })
   }
 })
